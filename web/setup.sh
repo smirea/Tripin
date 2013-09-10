9367a5cb1038bf63e8a7ee9b5a6f66b725b08d59
Stefan Mirea <steven.mirea@gmail.com>
2013-09-09 23:17:31 -0700

    implemented global node modules

--- web/setup.sh
+++ web/setup.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-prerequisites="node npm"
-node_modules="connect express socket.io orm sqlite3 fbgraph"
+prerequisites="node npm";
+node_modules="connect express socket.io orm sqlite3 fbgraph node-inspector";
+# requires sudo
+global_node_modules="node-inspector";
+
+cleanup () {
+  exit 1;
+}
+trap cleanup SIGTERM SIGINT
+
+if [ ${#global_node_modules[@]} -gt 0 ]; then
+  if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:";
+    echo "sudo $0 $*";
+    exit 1;
+  fi
+fi
 
 fail () {
   str=${1:-"Something went wrong"};
@@ -44,11 +59,6 @@ if [ ${#script_fails[@]} -gt 0 ]; then
 fi
 
 # install node modules with npm
-cleanup () {
-  exit 1;
-}
-trap cleanup SIGTERM SIGINT
-
 npm_fails=();
 for name in $node_modules; do
   echo ;
@@ -63,3 +73,22 @@ if [ ${#npm_fails[@]} -gt 0 ]; then
   echo
   fail " >> Failed to install the following npm packages: $npm_fails";
 fi
+
+# install global node modules with sudo npm -g
+global_npm_fails=();
+for name in $global_node_modules; do
+  echo ;
+  echo " >> Globally Installing: '$name'";
+  sudo npm -g install "$name";
+  if [ $? != 0 ]; then
+    global_npm_fails+=("$name");
+  fi
+done
+
+if [ ${#global_npm_fails[@]} -gt 0 ]; then
+  echo
+  fail " >> Failed to globally install the following npm packages: $global_npm_fails";
+fi
+
+echo "";
+echo "DONE!";
